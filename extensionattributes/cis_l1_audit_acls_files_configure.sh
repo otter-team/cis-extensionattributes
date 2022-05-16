ad4ac4e0be959b6a1d3926c067e75ad2c944328b
Peter Huerlimann <info@phnet.li>
2022-05-16 14:57:12 +0200

    script

--- extensionattributes/cis_l1_audit_acls_files_configure.sh
+++ extensionattributes/cis_l1_audit_acls_files_configure.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-## Checks audit_acls_files_configure
-## 
+## Checks audit_acls_files_configure based on the https://github.com/usnistgov/macos_security 
 
-cis_l1_audit_acls_files_configure=$(/bin/ls -le $(/usr/bin/grep '^dir' /etc/security/audit_control | /usr/bin/awk -F: '{print $2}') | /usr/bin/awk '{print $1}' | /usr/bin/grep -c ":"))
+cis_l1_audit_acls_files_configure=$(/bin/ls -le $(/usr/bin/grep '^dir' /etc/security/audit_control | /usr/bin/awk -F: '{print $2}') | /usr/bin/awk '{print $1}' | /usr/bin/grep -c ":")
 
-## if cis_l1_audit_acls_files_configure == 1 then, if cis_l1_audit_acls_files_configure == 0, else Failed check
+## Write results based on output
+
+if [ $cis_l1_audit_acls_files_configure == 0 ]; then
+    echo "<result>Secure</result>"
+elif [ $cis_l1_audit_acls_files_configure == 1 ]; then
+    echo "<result>Failure</result>"
+else
+    echo "<result>Error</result>"
+fi
