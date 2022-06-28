abef47b1dc8a3092d0ce7f57810bb34c73a77bf0
Peter <18031711+peterhuerlimann@users.noreply.github.com>
2022-06-28 10:48:56 +0200

    added comment

--- extensionattributes/cis_l1_audit_acls_files_configure.sh
+++ extensionattributes/cis_l1_audit_acls_files_configure.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-## Checks audit_acls_files_configure based on the https://github.com/usnistgov/macos_security 
+## Checks audit_acls_files_configure based on the https://github.com/usnistgov/macos_security project
 
 cis_l1_audit_acls_files_configure=$(/bin/ls -le $(/usr/bin/grep '^dir' /etc/security/audit_control | /usr/bin/awk -F: '{print $2}') | /usr/bin/awk '{print $1}' | /usr/bin/grep -c ":")
 
