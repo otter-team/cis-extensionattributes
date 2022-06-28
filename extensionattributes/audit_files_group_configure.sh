52072a4c658de539bc31123316925aced0b759f0
Peter <18031711+peterhuerlimann@users.noreply.github.com>
2022-06-28 12:52:52 +0200

    Update audit_files_group_configure.sh

--- extensionattributes/audit_files_group_configure.sh
+++ extensionattributes/audit_files_group_configure.sh
@@ -2,7 +2,7 @@
 
 ## Checks audit_files_group_configure based on the https://github.com/usnistgov/macos_security project
 
-cis_l1_audit_files_group_configure=$(  /bin/ls -n $(/usr/bin/grep '^dir' /etc/security/audit_control | /usr/bin/awk -F: '{print $2}') | /usr/bin/awk '{s+=$4} END {print s}')
+cis_l1_audit_files_group_configure=$(/bin/ls -n $(/usr/bin/grep '^dir' /etc/security/audit_control | /usr/bin/awk -F: '{print $2}') | /usr/bin/awk '{s+=$4} END {print s}')
 
 ## Write results based on output
 
